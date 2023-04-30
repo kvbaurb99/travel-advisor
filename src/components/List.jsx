9430b32961e72149de348928bac9d65f8d671d47
kvbaurb99 <kvbaurbanski2@gmail.com>
2023-04-30 22:26:16 +0200

    list component for filtering items

---
+++ src/components/List.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react'
+import Typography from '@mui/material/Typography'
+import { Box, FormControl, InputLabel, MenuItem, Select } from '@mui/material'
+import { ListControl } from '@/styles/materials'
+
+
+export default function List() {
+
+    const [type, setType] = useState('')
+    const [rating, setRating] = useState()
+
+  return (
+    <div className='p-4'>
+     <Typography variant="h4" color="initial">
+        Restaurants, Hotels & Attractions around you
+     </Typography>
+     <Box marginTop='1rem'>
+     <FormControl variant='standard' sx={{ width: '20%'}}>
+        <InputLabel>Type</InputLabel>
+        <Select label='Type' onChange={(e) => setType(e.target.value)}>
+            <MenuItem value='restaurants'>Restaurants</MenuItem>
+            <MenuItem value='hotels'>Hotels</MenuItem>
+            <MenuItem value='attractions'>Attractions</MenuItem>
+        </Select>
+     </FormControl>
+     <ListControl variant='standard' sx={{ width: '20%'}}>
+        <InputLabel>Rating</InputLabel>
+        <Select label='Rating' onChange={(e) => setRating(e.target.value)}>
+            <MenuItem value={0}>All</MenuItem>
+            <MenuItem value={3}>Above 3.0</MenuItem>
+            <MenuItem value={4}>Above 4.0</MenuItem>
+            <MenuItem value={4.5}>Above 4.5</MenuItem>
+        </Select>
+     </ListControl>
+     </Box>
+    </div>
+  )
+}
