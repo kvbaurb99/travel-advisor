943810e78c65ffe1742d3fbfca3332a6d48cefb3
kvbaurb99 <kvbaurbanski2@gmail.com>
2023-04-30 22:56:39 +0200

    state fixes

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -6,8 +6,8 @@ import { ListControl } from '@/styles/materials'
 
 export default function List() {
 
-    const [type, setType] = useState('')
-    const [rating, setRating] = useState()
+    const [type, setType] = useState('restaurants')
+    const [rating, setRating] = useState(0)
 
   return (
     <div className='p-4'>
@@ -17,7 +17,7 @@ export default function List() {
      <Box marginTop='1rem'>
      <FormControl variant='standard' sx={{ width: '20%'}}>
         <InputLabel>Type</InputLabel>
-        <Select label='Type' onChange={(e) => setType(e.target.value)}>
+        <Select value={type} label='Type' onChange={(e) => setType(e.target.value)}>
             <MenuItem value='restaurants'>Restaurants</MenuItem>
             <MenuItem value='hotels'>Hotels</MenuItem>
             <MenuItem value='attractions'>Attractions</MenuItem>
@@ -25,7 +25,7 @@ export default function List() {
      </FormControl>
      <ListControl variant='standard' sx={{ width: '20%'}}>
         <InputLabel>Rating</InputLabel>
-        <Select label='Rating' onChange={(e) => setRating(e.target.value)}>
+        <Select value={rating} label='Rating' onChange={(e) => setRating(e.target.value)}>
             <MenuItem value={0}>All</MenuItem>
             <MenuItem value={3}>Above 3.0</MenuItem>
             <MenuItem value={4}>Above 4.0</MenuItem>
