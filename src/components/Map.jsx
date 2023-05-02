28f143d3e6673fc1915ca3a3619917f8401a6706
kvbaurb99 <kvbaurbanski2@gmail.com>
2023-05-02 21:37:35 +0200

    displaying places on map

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import GoogleMapReact from 'google-map-react'
 import { Paper, Typography, useMediaQuery } from '@mui/material'
+import { MdLocationPin } from 'react-icons/md'
+import Image from 'next/image';
 
-export default function Map({ handleBounds, handleCoordinates, coordinates}) {
+export default function Map({ handleBounds, handleCoordinates, coordinates, places}) {
 
+  const isMobile = useMediaQuery('(max-width:600px)');
 
   return (
     <div className='h-screen w-full ml-6'>
@@ -16,10 +19,36 @@ export default function Map({ handleBounds, handleCoordinates, coordinates}) {
             options={''}
             onChange={(e) => {
                 handleCoordinates({ lat: e.center.lat, lng: e.center.lng })
-                handleBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw })
+                if (e.marginBounds) {
+                    handleBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw })
+                  }
             }}
             onChildClick={''}
         >
+          {places?.map((place, i) => (
+            <div
+              className='w-[150px] hover:relative hover:z-50 cursor-pointer'
+              lat={Number(place.latitude)}
+              lng={Number(place.longitude)}
+              key={i}
+            >
+              {
+                isMobile ? (
+                  <MdLocationPin />
+                ) : (
+                  <Paper elevation={8} sx={{ padding: '15px'}}>
+                    <p className='text-center text-lg'>{place.name}</p>
+                    {place.photo ? <Image
+                      src={place?.photo?.images?.large?.url}
+                      width={125}
+                      height={100}
+                      className='max-h-[80px] mx-auto'
+                    /> : null}
+                  </Paper>
+                )
+              }
+            </div>
+          ))}
         </GoogleMapReact>
     </div>
   )
